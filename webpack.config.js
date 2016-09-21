5aa5e188a7d85fc265558489a9f03c22b8f26c0b
Veen <bveenema@gmail.com>
2016-09-21 06:10:38 -0400

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	entry: './app.jsx',
+
+	output: {
+		//path: 'build',
+		filename: 'bundle.js'
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			}
+		],
+	},
+};
